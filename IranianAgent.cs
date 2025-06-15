4009d6628ce8ad426b98b716807904ebd385517d
Arie <s7925al@gmail.com>
2025-06-15 15:53:18 +0300

    add_game

---
+++ IranianAgent.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InvestigationGame.models
+{
+    public class IranianAgent
+    {
+        public string Name;
+        public string Rank;
+        public List<string> Sensor;
+
+        public IranianAgent(string name, string rank,List<string>sensor)
+        {
+            Name = name;
+            Rank = rank;
+            Sensor = sensor;
+        }
+       
+    }
+}
